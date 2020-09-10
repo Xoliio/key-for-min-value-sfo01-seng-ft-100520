d8a47a3fbbda01c94642408ba76582580b602877
Inee Ader <xoliio@gmail.com>
2020-09-10 22:33:43 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,13 +2,11 @@
 # Given a hash with numeric values, return the key for the smallest value
 
   def key_for_min_value(name_hash)
-    person_name = nil
-    person_value = nil
-    name_hash.each do |person, value|
-      if person_value == nil || value < person_value
-        person_value = value
-        person_name = person
+    name_hash.each do |name|
+
+      name.each do |value|
+        
+
       end
     end
-    person_name
   end

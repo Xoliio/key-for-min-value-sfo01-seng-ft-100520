6a8f507dd951e3893c786098eafa1d921c69341f
Inee Ader <xoliio@gmail.com>
2020-09-10 22:33:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,11 +1,12 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+require "pry"
 
   def key_for_min_value(name_hash)
     name_hash.each do |name|
 
       name.each do |value|
-        
+
 
       end
     end

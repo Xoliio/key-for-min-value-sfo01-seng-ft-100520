efa18dfd62df90514e67b692ec55848fa12591ca
Inee Ader <xoliio@gmail.com>
2020-09-10 22:34:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,9 +4,9 @@ require "pry"
 
   def key_for_min_value(name_hash)
     name_hash.each do |name|
-      binding.pry
-      name.each do |value|
 
+      name.each do |value|
+binding.pry
 
       end
     end
